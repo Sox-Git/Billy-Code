a094654a7c218285f2f5cc83d8efe3d8019fcc92
Sox <113142608+Maz-github@users.noreply.github.com>
2024-10-04 21:33:33 -0400

    ThemeTest

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -9,9 +9,10 @@ class PlayState extends FlxState
 	{
 		super.create();
 		var TestBG1 = new FlxSprite('images/Menu/Main/bg');
-
 		TestBG1.screenCenter();
 		add(TestBG1);
+
+		FlxG.music.play(Paths.music('BB1/TitleTheme'));
 	}
 
 	override public function update(elapsed:Float)
